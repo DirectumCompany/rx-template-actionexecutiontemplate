e7f2edab5a1f94355ea010d7a41cc42aee714381
Tikhonov_EM <Tikhonov_EM@directum.ru>
2022-05-27 17:41:22 +0400

    Адаптация под 4.3

--- GD.ActionTemplateModule/GD.ActionTemplateModule.ClientBase/ModuleClientFunctions.cs
+++ GD.ActionTemplateModule/GD.ActionTemplateModule.ClientBase/ModuleClientFunctions.cs
@@ -13,22 +13,37 @@ namespace GD.ActionTemplateModule.Client
   {
 
     /// <summary>
-    /// Сохрнаить данные поручения в шаблон
+    /// Сохранить данные поручения в шаблон
     /// </summary>
     [Public]
     public void ToTemplate(IActionItemExecutionTask task)
     {
       var template = ActionTemplateModule.PublicFunctions.AssignmentsTemplate.Remote.CreateTemplate();
-      template.Text = task.ActionItem;
+      template.IsCompoundActionItem = task.IsCompoundActionItem;
+      template.Text = task.ActiveText;
       template.AssignedBy = task.AssignedBy;
       template.IsUnderControl = task.IsUnderControl;
       template.Supervisor = task.Supervisor;
-      template.Assignee = task.Assignee;
-      foreach (var coAssignees in task.CoAssignees)
+      template.HasIndefiniteDeadline = task.HasIndefiniteDeadline;
+      
+      if (task.IsCompoundActionItem == true)
       {
-        var newCoAssignee = template.CoAssignees.AddNew();
-        newCoAssignee.CoAssignee = coAssignees.Assignee;
+        foreach (var actionItemPart in task.ActionItemParts)
+        {
+          var coAssignees = task.PartsCoAssignees.Where(p => p.PartGuid == actionItemPart.PartGuid).Select(p => p.CoAssignee).ToList();
+          AddActionItemPart(template, actionItemPart.Assignee, actionItemPart.ActionItemPart, coAssignees, actionItemPart.Supervisor);
+        }
       }
+      else
+      {
+        template.Assignee = task.Assignee;
+        foreach (var coAssignees in task.CoAssignees)
+        {
+          var newCoAssignee = template.CoAssignees.AddNew();
+          newCoAssignee.CoAssignee = coAssignees.Assignee;
+        }
+      }
+      
       template.Show();
     }
     
@@ -41,12 +56,76 @@ namespace GD.ActionTemplateModule.Client
       var template = ActionTemplateModule.PublicFunctions.AssignmentsTemplate.Remote.GetAvailableTemplates().ShowSelect();
       if (template != null)
       {
-        task.ActionItem = template.Text;
+        task.IsCompoundActionItem = template.IsCompoundActionItem;
+        task.ActiveText = template.Text;
         task.AssignedBy = template.AssignedBy;
         task.IsUnderControl = template.IsUnderControl;
         task.Supervisor = template.Supervisor;
-        task.Assignee = template.Assignee;
+        task.HasIndefiniteDeadline = template.HasIndefiniteDeadline;
         
+        if (template.IsCompoundActionItem == true)
+          FillCompoundActionItemProperties(task, template);
+        else
+          FillSimpleActionItemProperties(task, template);
+      }
+    }
+    
+    public void FillCompoundActionItemProperties(IActionItemExecutionTask task, IAssignmentsTemplate template)
+    {
+      var hasNotIndefiniteDeadline = template.HasIndefiniteDeadline == false;
+      
+      if (hasNotIndefiniteDeadline)
+      {
+        if (template.FinalDaysOrHours != null && template.FinalCount != null)
+        {
+          if (template.FinalDaysOrHours == AssignmentsTemplate.FinalDaysOrHours.Days)
+            task.FinalDeadline = Calendar.Today.AddWorkingDays(template.FinalCount.Value);
+          else
+            task.FinalDeadline = Calendar.Now.AddWorkingHours(template.FinalCount.Value);
+        }
+      }
+      
+      task.ActionItemParts.Clear();
+      task.PartsCoAssignees.Clear();
+      foreach (var actionItemPart in template.ActionItemParts)
+      {
+        DateTime? assigneeDeadline = null;
+        if (hasNotIndefiniteDeadline && actionItemPart.DaysOrHours != null && actionItemPart.Count != null)
+        {
+          if (actionItemPart.DaysOrHours == AssignmentsTemplateActionItemParts.DaysOrHours.Days)
+            assigneeDeadline = Calendar.Today.AddWorkingDays(actionItemPart.Count.Value);
+          else
+            assigneeDeadline = Calendar.Now.AddWorkingHours(actionItemPart.Count.Value);
+        }
+        
+        DateTime? coAssigneesDeadline = null;
+        if (hasNotIndefiniteDeadline && actionItemPart.CoAssigneesCount != null && actionItemPart.CoAssigneesDaysOrHours != null)
+        {
+          if (actionItemPart.CoAssigneesDaysOrHours == AssignmentsTemplateActionItemParts.CoAssigneesDaysOrHours.Days)
+            coAssigneesDeadline = Calendar.Today.AddWorkingDays(actionItemPart.CoAssigneesCount.Value);
+          else
+            coAssigneesDeadline = Calendar.Now.AddWorkingHours(actionItemPart.CoAssigneesCount.Value);
+        }
+        
+        var coAssignees = template.PartsCoAssignees.Where(p => p.PartGuid == actionItemPart.PartGuid).Select(p => p.CoAssignee).ToList();
+        
+        Sungero.RecordManagement.PublicFunctions.ActionItemExecutionTask.AddActionItemPart(task,
+                                                                                           actionItemPart.Assignee,
+                                                                                           assigneeDeadline,
+                                                                                           actionItemPart.ActionItemPart,
+                                                                                           coAssignees,
+                                                                                           coAssigneesDeadline,
+                                                                                           actionItemPart.Supervisor);
+      }
+    }
+    
+    public void FillSimpleActionItemProperties(IActionItemExecutionTask task, IAssignmentsTemplate template)
+    {
+      var hasNotIndefiniteDeadline = template.HasIndefiniteDeadline == false;
+      
+      task.Assignee = template.Assignee;
+      if (hasNotIndefiniteDeadline)
+      {
         if (template.DaysOrHours != null && template.Count != null)
         {
           if (template.DaysOrHours == AssignmentsTemplate.DaysOrHours.Days)
@@ -55,13 +134,58 @@ namespace GD.ActionTemplateModule.Client
             task.Deadline = Calendar.Now.AddWorkingHours(template.Count.Value);
         }
         
-        task.CoAssignees.Clear();
-        foreach (var coAssignees in template.CoAssignees)
+        if (template.CoAssigneesCount != null && template.CoAssigneesDaysOrHours != null)
         {
-          var newCoAssignee = task.CoAssignees.AddNew();
-          newCoAssignee.Assignee = coAssignees.CoAssignee;
+          if (template.CoAssigneesDaysOrHours == AssignmentsTemplate.CoAssigneesDaysOrHours.Days)
+            task.CoAssigneesDeadline = Calendar.Today.AddWorkingDays(template.CoAssigneesCount.Value);
+          else
+            task.CoAssigneesDeadline = Calendar.Now.AddWorkingHours(template.CoAssigneesCount.Value);
         }
       }
+      
+      task.CoAssignees.Clear();
+      foreach (var coAssignees in template.CoAssignees)
+      {
+        var newCoAssignee = task.CoAssignees.AddNew();
+        newCoAssignee.Assignee = coAssignees.CoAssignee;
+      }
     }
+    
+    /// <summary>
+    /// Добавить пункт порученияв шаблон.
+    /// </summary>
+    /// <param name="template">Шаблон.</param>
+    /// <param name="assignee">Исполнитель.</param>
+    /// <param name="actionItemPart">Пункт поручения.</param>
+    /// <param name="coAssignees">Соисполнители.</param>
+    /// <param name="supervisor">Контролер.</param>
+    public void AddActionItemPart(IAssignmentsTemplate template, Sungero.Company.IEmployee assignee, string actionItemPart,
+                                  List<Sungero.Company.IEmployee> coAssignees, Sungero.Company.IEmployee supervisor)
+    {
+      var actionItem = template.ActionItemParts.AddNew();
+      actionItem.ActionItemPart = actionItemPart;
+      actionItem.Assignee = assignee;
+      actionItem.Supervisor = supervisor;
+      AddPartsCoAssignees(template, actionItem, coAssignees);
+    }
+    
+    /// <summary>
+    /// Добавить соисполнителей для пункта поручения.
+    /// </summary>
+    /// <param name="template">Шаблон.</param>
+    /// <param name="actionItemPart">Пункт поручения.</param>
+    /// <param name="coAssignees">Соисполнители.</param>
+    public void AddPartsCoAssignees(IAssignmentsTemplate template, IAssignmentsTemplateActionItemParts actionItemPart, List<Sungero.Company.IEmployee> coAssignees)
+    {
+      foreach (var coAssignee in coAssignees)
+      {
+        var item = template.PartsCoAssignees.AddNew();
+        item.CoAssignee = coAssignee;
+        item.PartGuid = actionItemPart.PartGuid;
+      }
+      
+      actionItemPart.CoAssignees = Sungero.Docflow.PublicFunctions.Module.GetCoAssigneesNames(coAssignees, true);
+    }
+    
   }
 }
--- GD.ActionTemplateModule/GD.ActionTemplateModule.Server/AssignmentsTemplate/AssignmentsTemplateServerFunctions.cs
+++ GD.ActionTemplateModule/GD.ActionTemplateModule.Server/AssignmentsTemplate/AssignmentsTemplateServerFunctions.cs
@@ -15,7 +15,7 @@ namespace GD.ActionTemplateModule.Server
     [Public, Remote(PackResultEntityEagerly = true)]
     public static IAssignmentsTemplate CreateTemplate()
     {
-      return  AssignmentsTemplates.Create();
+      return AssignmentsTemplates.Create();
     }
     
     /// <summary>
@@ -25,7 +25,7 @@ namespace GD.ActionTemplateModule.Server
     [Public, Remote(IsPure = true)]
     public static IQueryable<IAssignmentsTemplate> GetAvailableTemplates()
     {
-      return AssignmentsTemplates.GetAll();
+      return AssignmentsTemplates.GetAll(t => t.Status == Status.Active);
     }
   }
 }
